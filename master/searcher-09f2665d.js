62634b80fd9e231bbbbfd1a0fe25cd08668e1090
gj1118 <gj1118@users.noreply.github.com>
2026-07-04 23:21:52 +0000

    deploy: 4b02fed2eb585d849be92a52fb181759028599ec

--- master/searcher-09f2665d.js
+++ master/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1de5e215.js',
+                path_to_root + 'searchindex-ef85d442.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
